a52f44f6bdf622a9ee2fc03ecccbf8cb1f5523a5
Home <home@Matilda.local>
2012-06-15 12:32:22 +0400

    Initial Commit

---
+++ jellyphysics/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  jellyphysics
+//
+//  Created by Home on 15/06/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
